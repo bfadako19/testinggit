0671cfd0d2e8ce6c25d1db21989fe4487fef9e47
Bryce Fadako <fadakob@chc.edu>
2023-02-23 10:22:29 -0500

    Version 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Countdown from "react-countdown";
-const Completionist = () => <span>You are good to go!</span>;
+const Completionist = () => <span style={{color: 'red', fontWeight: 'bold', fontSize: 52}}>You are good to go!</span>;
 const renderer = ({hours, minutes, seconds, completed}) => {
   if(completed){
     return <Completionist/>;
@@ -10,9 +10,21 @@ const renderer = ({hours, minutes, seconds, completed}) => {
 }
 function App() {
   return (
-   <div>
+   <div style={{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: 52,
+    height: '100vh',
+    overflow: 'auto'
+   }}>
     <h2>Countdown</h2>
-    <div>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
       <Countdown
       date={Date.now() +10000}
       renderer = {renderer}
